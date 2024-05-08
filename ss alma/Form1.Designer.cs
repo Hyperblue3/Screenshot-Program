1bc2f69cb432fa2fd21e41f63976413d6907b542
Hyperblue3 <tanisikvolkan@gmail.com>
2024-05-08 17:42:38 +0300

    First commit

---
+++ ss alma/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ss_alma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 96);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SS al";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(119, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 96);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Çıkış";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 96);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Home tusu ss alır";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ss alma/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ss_alma
+{
+    public partial class Form1 : Form
+    {
+        string KayitYolu = @"C:\ssler";
+        string ResimAdi()
+        {
+            return "SS_" + DateTime.Now.ToString().Replace(" ", "_").Replace(":", "_") + ".jpg";
+
+
+        }
+        void Kaydet(string resimadi)
+        {
+            if (!File.Exists(KayitYolu))
+                Directory.CreateDirectory(KayitYolu);
+            Screenshot().Save(KayitYolu + "\\" + resimadi + "", ImageFormat.Jpeg);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Home)
+                Kaydet(ResimAdi());
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kaydet(ResimAdi());
+        }
+        private void button2_Click(object sender, EventArgs e) { 
+        Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private Bitmap Screenshot()
+        {
+            this.Opacity = 0;
+            //ss alan kodlar
+            Bitmap Screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics GFX = Graphics.FromImage(Screenshot);
+            GFX.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+
+            this.Opacity = 1;
+            return Screenshot;
+
+        }
+
+      
+    }
+}
